fe45bec8a8296e39a6886456f280188366101bb8
Yann Olive <yanolive@2C3.42angouleme.fr>
2026-02-03 13:46:02 +0100

    fix ex03 and 04 and begin of ex05

---
+++ 1-Array/ex05/pimp_image.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def ft_invert(array) -> array:
+
+def ft_red(array) -> array:
+
+def ft_green(array) -> array:
+
+def ft_blue(array) -> array:
+
+def ft_grey(array) -> array:
