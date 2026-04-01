08982c906c21c4bddf8acf17f865f2a4901a5f26
Yann Olive <yanolive@2D7.42angouleme.fr>
2026-04-01 16:44:09 +0200

    ex05 array filters

--- 1-Array/ex05/pimp_image.py
+++ 1-Array/ex05/pimp_image.py
@@ -1,13 +1,22 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 
-def ft_invert(array) -> array:
+def ft_invert(array) -> np.array:
+    invert_array = np.array([[[255 - array[i, j, 0], 255 - array[i, j, 1], 255 - array[i, j, 2]] for j in range(array.shape[1])] for i in range(array.shape[0])])
+    return invert_array
 
-def ft_red(array) -> array:
+def ft_red(array) -> np.array:
+    red_array = np.array([[[array[i, j, 0], 0, 0] for j in range(array.shape[1])] for i in range(array.shape[0])])
+    return red_array
 
-def ft_green(array) -> array:
+def ft_green(array) -> np.array:
+    green_array = np.array([[[0, array[i, j, 1], 0] for j in range(array.shape[1])] for i in range(array.shape[0])])
+    return green_array
 
-def ft_blue(array) -> array:
+def ft_blue(array) -> np.array:
+    blue_array = np.array([[[0, 0, array[i, j, 2]] for j in range(array.shape[1])] for i in range(array.shape[0])])
+    return blue_array
 
-def ft_grey(array) -> array:
+def ft_grey(array) -> np.array:
+    # grey_array = np.array([[[int(array[i, j, 0] / 3), int(array[i, j, 1] / 3), int(array[i, j, 2] / 3)] for j in range(array.shape[1])] for i in range(array.shape[0])])
+    return array[:, :, 1]
--- 1-Array/ex05/tester.py
+++ 1-Array/ex05/tester.py
@@ -1,16 +1,20 @@
 from load_image import ft_load
-from pimp_image import ft_invert
-from pimp_image import ft_red
-from pimp_image import ft_green
-from pimp_image import ft_blue
-from pimp_image import ft_grey
+from pimp_image import ft_invert, ft_red, ft_green, ft_blue, ft_grey
+from numpy import array
+import matplotlib.pyplot as plt
 
-array = ft_load("landscape.jpg")
+image = ft_load("landscape.jpg")
+image_array = array(image)
 
-ft_invert(array)
-ft_red(array)
-ft_green(array)
-ft_blue(array)
-ft_grey(array)
+plt.imshow(ft_invert(image_array))
+plt.show()
+plt.imshow(ft_red(image_array))
+plt.show()
+plt.imshow(ft_green(image_array))
+plt.show()
+plt.imshow(ft_blue(image_array))
+plt.show()
+plt.imshow(ft_grey(image_array), cmap='grey')
+plt.show()
 
 print(ft_invert.__doc__)
